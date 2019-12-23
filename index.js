bb2bfc84853f468005267cd274294bdc749609f5
Kelly Bondanza <kelly.bondanza@gmail.com>
2019-12-23 18:44:36 -0500

    installed styled-system and styled-components, scaffolding out files

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+
+const mountNode = document.getElementById("app");
+ReactDOM.render(<App />, mountNode);
